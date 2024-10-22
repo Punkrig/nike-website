b5a8372040921f700b24155b04fc22ac0730664c
Punkrig <igorbrandaao@gmail.com>
2024-10-22 18:25:27 -0300

    Desenvolvimento: Estrutura da página          - Criando a estrutura

---
+++ src/sections/index.js
@@ -0,0 +1,12 @@
+import Hero from "./sections/Hero";
+import PopularProducts from "./sections/PopularProducts";
+import SuperQuality from "./sections/SuperQuality";
+import Services from "./sections/Services";
+import SpecialOffer from "./sections/SpecialOffers";
+import CustomerReviews from "./sections/CustomerReviews";
+import Subscribe from "./sections/Subscribe";
+import Footer from "./sections/Footer";
+
+export{
+    Hero, Services, SpecialOffer, CustomerReviews, Subscribe, Footer
+}
