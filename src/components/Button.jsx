f9fb4838cd886ef7d088ff0b6fbadee70b2c78dc
Punkrig <igorbrandaao@gmail.com>
2024-10-25 17:31:46 -0300

    Desenvolvimento: Ofertas especiais            - Desenvolvendo a seção de ofertas especiais da página

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,20 +1,26 @@
 
-const Button = ({label,iconUrl}) => {
+const Button = ({label,iconUrl, backgroundColor,
+  textColor, borderColor
+}) => {
   return (
-    <button className="flex
+    <button className={`flex
     justify-center items-center
     gap-2 px-7 py-4 border font-montserrat
-    text-lg leading-none bg-coral-red
-    rounded-full text-white
-    border-coral-red">
+    text-lg leading-none
+    ${
+      backgroundColor
+      ? `${backgroundColor} ${textColor} ${borderColor}`
+      :"bg-coral-red  border-coral-red text-white"
+    } rounded-full "}`}
+    >
         {label}
 
-        <img 
+        {iconUrl && <img 
         src={iconUrl} 
         alt="arrow rigth icon" 
         className="ml-2 rounded-full
         w-5 h-5"
-        />
+        />}
     </button>
 )
 }
